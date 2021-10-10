0fe7acf4f5d929ee73b2671a75d9cde4ecead618
Brian Weiss <brianmweiss@gmail.com>
2021-10-10 13:38:34 -0400

    Stop using JAVA_OPTS in run.sh

--- src/main/non-packaged-resources/run.sh
+++ src/main/non-packaged-resources/run.sh
@@ -3,6 +3,4 @@
 # Uncomment the following line to set the bot's base directory, or set this in your shell's rc file.
 #export MORTYBOT_HOME=/path/to/mortybot
 
-export JAVA_OPTS="-Dlogback.configurationFile=conf/logback.xml"
-
-java $JAVA_OPTS -jar mortybot.jar
+java -Dlogback.configurationFile=conf/logback.xml -jar mortybot.jar
