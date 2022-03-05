569d3c6d0094aa5ab4c79513f6c6103e8dd9a72e
Brian Weiss <brianmweiss@gmail.com>
2022-03-04 22:12:33 -0500

    MortyBot 0.5.0

--- src/main/java/net/hatemachine/mortybot/MortyBot.java
+++ src/main/java/net/hatemachine/mortybot/MortyBot.java
@@ -42,7 +42,7 @@ import java.util.Objects;
 
 public class MortyBot extends PircBotX {
 
-    public static final String VERSION = "0.4.2";
+    public static final String VERSION = "0.5.0";
 
     private static final Logger log = LoggerFactory.getLogger(MortyBot.class);
 
