55f573232ad287fe45987623eb09fe258bd144af
Brian Weiss <brianmweiss@gmail.com>
2022-03-09 19:15:07 -0500

    comments

--- src/main/non-packaged-resources/run.bat
+++ src/main/non-packaged-resources/run.bat
@@ -1,4 +1,4 @@
-rem Uncomment the following line to set the bot's home directory
+rem Uncomment the following line to set the bot's base directory. Otherwise it will attempt to autodetect.
 rem set MORTYBOT_HOME=C:\path\to\mortybot
 
 java -Dlogback.configurationFile=conf\logback.xml -jar mortybot.jar
--- src/main/non-packaged-resources/run.sh
+++ src/main/non-packaged-resources/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# Uncomment the following line to set the bot's base directory, or set this in your shell's rc file.
+# Uncomment the following line to set the bot's base directory. Otherwise it will attempt to autodetect.
 #export MORTYBOT_HOME=/path/to/mortybot
 
 java -Dlogback.configurationFile=conf/logback.xml -jar mortybot.jar
