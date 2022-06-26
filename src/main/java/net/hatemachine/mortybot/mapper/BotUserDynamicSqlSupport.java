70a551f59fd8a061e3fda2469ed8cbbcc1531bbd
Brian Weiss <brianmweiss@gmail.com>
2022-06-26 16:22:13 -0400

    Add zipCode field to BotUser model
    This can be used to allow users to set default zip codes for weather lookups.

--- src/main/java/net/hatemachine/mortybot/mapper/BotUserDynamicSqlSupport.java
+++ src/main/java/net/hatemachine/mortybot/mapper/BotUserDynamicSqlSupport.java
@@ -6,22 +6,25 @@ import org.mybatis.dynamic.sql.AliasableSqlTable;
 import org.mybatis.dynamic.sql.SqlColumn;
 
 public final class BotUserDynamicSqlSupport {
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9473977-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4646178-04:00", comments="Source Table: bot_users")
     public static final BotUser botUser = new BotUser();
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9483976-04:00", comments="Source field: bot_users.bot_user_id")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4646178-04:00", comments="Source field: bot_users.bot_user_id")
     public static final SqlColumn<Integer> botUserId = botUser.botUserId;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9483976-04:00", comments="Source field: bot_users.username")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4646178-04:00", comments="Source field: bot_users.username")
     public static final SqlColumn<String> username = botUser.username;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9494342-04:00", comments="Source field: bot_users.hostmasks")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4656175-04:00", comments="Source field: bot_users.hostmasks")
     public static final SqlColumn<String> hostmasks = botUser.hostmasks;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9494342-04:00", comments="Source field: bot_users.flags")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4656175-04:00", comments="Source field: bot_users.flags")
     public static final SqlColumn<String> flags = botUser.flags;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9483976-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4656175-04:00", comments="Source field: bot_users.zip_code")
+    public static final SqlColumn<String> zipCode = botUser.zipCode;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4646178-04:00", comments="Source Table: bot_users")
     public static final class BotUser extends AliasableSqlTable<BotUser> {
         public final SqlColumn<Integer> botUserId = column("bot_user_id", JDBCType.INTEGER);
 
@@ -30,6 +33,8 @@ public final class BotUserDynamicSqlSupport {
         public final SqlColumn<String> hostmasks = column("hostmasks", JDBCType.VARCHAR);
 
         public final SqlColumn<String> flags = column("flags", JDBCType.VARCHAR);
+
+        public final SqlColumn<String> zipCode = column("zip_code", JDBCType.VARCHAR);
 
         public BotUser() {
             super("bot_users", BotUser::new);
--- src/main/java/net/hatemachine/mortybot/mapper/BotUserMapper.java
+++ src/main/java/net/hatemachine/mortybot/mapper/BotUserMapper.java
@@ -31,130 +31,138 @@ import org.mybatis.dynamic.sql.util.mybatis3.MyBatis3Utils;
 
 @Mapper
 public interface BotUserMapper extends CommonCountMapper, CommonDeleteMapper, CommonInsertMapper<BotUser>, CommonUpdateMapper {
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9810598-04:00", comments="Source Table: bot_users")
-    BasicColumn[] selectList = BasicColumn.columnList(botUserId, username, hostmasks, flags);
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4746184-04:00", comments="Source Table: bot_users")
+    BasicColumn[] selectList = BasicColumn.columnList(botUserId, username, hostmasks, flags, zipCode);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9514346-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4656175-04:00", comments="Source Table: bot_users")
     @SelectProvider(type=SqlProviderAdapter.class, method="select")
     @Results(id="BotUserResult", value = {
         @Result(column="bot_user_id", property="botUserId", jdbcType=JdbcType.INTEGER, id=true),
         @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
         @Result(column="hostmasks", property="hostmasks", jdbcType=JdbcType.VARCHAR),
-        @Result(column="flags", property="flags", jdbcType=JdbcType.VARCHAR)
+        @Result(column="flags", property="flags", jdbcType=JdbcType.VARCHAR),
+        @Result(column="zip_code", property="zipCode", jdbcType=JdbcType.VARCHAR)
     })
     List<BotUser> selectMany(SelectStatementProvider selectStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9574013-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4676161-04:00", comments="Source Table: bot_users")
     @SelectProvider(type=SqlProviderAdapter.class, method="select")
     @ResultMap("BotUserResult")
     Optional<BotUser> selectOne(SelectStatementProvider selectStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9584235-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4676161-04:00", comments="Source Table: bot_users")
     default long count(CountDSLCompleter completer) {
         return MyBatis3Utils.countFrom(this::count, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9593942-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4686176-04:00", comments="Source Table: bot_users")
     default int delete(DeleteDSLCompleter completer) {
         return MyBatis3Utils.deleteFrom(this::delete, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9663988-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4686176-04:00", comments="Source Table: bot_users")
     default int deleteByPrimaryKey(Integer botUserId_) {
         return delete(c -> 
             c.where(botUserId, isEqualTo(botUserId_))
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.969156-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4696181-04:00", comments="Source Table: bot_users")
     default int insert(BotUser row) {
         return MyBatis3Utils.insert(this::insert, row, botUser, c ->
             c.map(botUserId).toProperty("botUserId")
             .map(username).toProperty("username")
             .map(hostmasks).toProperty("hostmasks")
             .map(flags).toProperty("flags")
+            .map(zipCode).toProperty("zipCode")
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.972924-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4716175-04:00", comments="Source Table: bot_users")
     default int insertMultiple(Collection<BotUser> records) {
         return MyBatis3Utils.insertMultiple(this::insertMultiple, records, botUser, c ->
             c.map(botUserId).toProperty("botUserId")
             .map(username).toProperty("username")
             .map(hostmasks).toProperty("hostmasks")
             .map(flags).toProperty("flags")
+            .map(zipCode).toProperty("zipCode")
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9766512-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4726172-04:00", comments="Source Table: bot_users")
     default int insertSelective(BotUser row) {
         return MyBatis3Utils.insert(this::insert, row, botUser, c ->
             c.map(botUserId).toPropertyWhenPresent("botUserId", row::getBotUserId)
             .map(username).toPropertyWhenPresent("username", row::getUsername)
             .map(hostmasks).toPropertyWhenPresent("hostmasks", row::getHostmasks)
             .map(flags).toPropertyWhenPresent("flags", row::getFlags)
+            .map(zipCode).toPropertyWhenPresent("zipCode", row::getZipCode)
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9820545-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4756187-04:00", comments="Source Table: bot_users")
     default Optional<BotUser> selectOne(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectOne(this::selectOne, selectList, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9832561-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4766178-04:00", comments="Source Table: bot_users")
     default List<BotUser> select(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectList(this::selectMany, selectList, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.983771-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4766178-04:00", comments="Source Table: bot_users")
     default List<BotUser> selectDistinct(SelectDSLCompleter completer) {
         return MyBatis3Utils.selectDistinct(this::selectMany, selectList, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9857869-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4776176-04:00", comments="Source Table: bot_users")
     default Optional<BotUser> selectByPrimaryKey(Integer botUserId_) {
         return selectOne(c ->
             c.where(botUserId, isEqualTo(botUserId_))
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9867899-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4776176-04:00", comments="Source Table: bot_users")
     default int update(UpdateDSLCompleter completer) {
         return MyBatis3Utils.update(this::update, botUser, completer);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9877899-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4786184-04:00", comments="Source Table: bot_users")
     static UpdateDSL<UpdateModel> updateAllColumns(BotUser row, UpdateDSL<UpdateModel> dsl) {
         return dsl.set(botUserId).equalTo(row::getBotUserId)
                 .set(username).equalTo(row::getUsername)
                 .set(hostmasks).equalTo(row::getHostmasks)
-                .set(flags).equalTo(row::getFlags);
+                .set(flags).equalTo(row::getFlags)
+                .set(zipCode).equalTo(row::getZipCode);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9907881-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4786184-04:00", comments="Source Table: bot_users")
     static UpdateDSL<UpdateModel> updateSelectiveColumns(BotUser row, UpdateDSL<UpdateModel> dsl) {
         return dsl.set(botUserId).equalToWhenPresent(row::getBotUserId)
                 .set(username).equalToWhenPresent(row::getUsername)
                 .set(hostmasks).equalToWhenPresent(row::getHostmasks)
-                .set(flags).equalToWhenPresent(row::getFlags);
+                .set(flags).equalToWhenPresent(row::getFlags)
+                .set(zipCode).equalToWhenPresent(row::getZipCode);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9937946-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4796201-04:00", comments="Source Table: bot_users")
     default int updateByPrimaryKey(BotUser row) {
         return update(c ->
             c.set(username).equalTo(row::getUsername)
             .set(hostmasks).equalTo(row::getHostmasks)
             .set(flags).equalTo(row::getFlags)
+            .set(zipCode).equalTo(row::getZipCode)
             .where(botUserId, isEqualTo(row::getBotUserId))
         );
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T12:33:00.9977866-04:00", comments="Source Table: bot_users")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-06-26T16:25:50.4806189-04:00", comments="Source Table: bot_users")
     default int updateByPrimaryKeySelective(BotUser row) {
         return update(c ->
             c.set(username).equalToWhenPresent(row::getUsername)
             .set(hostmasks).equalToWhenPresent(row::getHostmasks)
             .set(flags).equalToWhenPresent(row::getFlags)
+            .set(zipCode).equalToWhenPresent(row::getZipCode)
             .where(botUserId, isEqualTo(row::getBotUserId))
         );
     }
