34a8ac6a25a9959f98d16c6e0a0fa8473d6a8b19
Brian Weiss <brianmweiss@gmail.com>
2023-03-07 18:06:07 -0500

    ProgressBar refactoring
    Most importantly, we now round the number of filled units to the nearest integer.

--- src/main/java/net/hatemachine/mortybot/util/ProgressBar.java
+++ src/main/java/net/hatemachine/mortybot/util/ProgressBar.java
@@ -17,50 +17,83 @@
  */
 package net.hatemachine.mortybot.util;
 
+/**
+ * A class to represent an ASCII progress bar that can vary in size.
+ */
 public class ProgressBar {
 
     private static final char FILLED_CHAR_DEFAULT = '\u2593';
     private static final char UNFILLED_CHAR_DEFAULT = '\u2591';
 
     private final int size;
-    private final double unitVal;
     private final char filledChar;
     private final char unfilledChar;
     private final boolean showPercentageFlag;
 
+    /**
+     * Creates a progress bar of a certain size with the default fill characters.
+     *
+     * @param size the total size of the progress bar in characters
+     */
     public ProgressBar(int size) {
-        this(size, FILLED_CHAR_DEFAULT, UNFILLED_CHAR_DEFAULT, false);
+        this(size, FILLED_CHAR_DEFAULT, UNFILLED_CHAR_DEFAULT);
     }
 
+    /**
+     * Creates a progress bar of a certain size with the default fill characters.
+     *
+     * @param size the total size of the progress bar in characters
+     * @param showPercentageFlag if true the percentage will be included after the progress bar
+     */
     public ProgressBar(int size, boolean showPercentageFlag) {
         this(size, FILLED_CHAR_DEFAULT, UNFILLED_CHAR_DEFAULT, showPercentageFlag);
     }
 
+    /**
+     * Creates a progress bar of a certain size with the specified fill characters.
+     *
+     * @param size the total size of the progress bar in characters
+     * @param filledChar the character used to represent a filled unit of the bar
+     * @param unfilledChar the character used to represent an unfilled unit of the bar
+     */
     public ProgressBar(int size, char filledChar, char unfilledChar) {
         this(size, filledChar, unfilledChar, false);
     }
 
+    /**
+     * Creates a progress bar of a certain size with the specified fill characters.
+     *
+     * @param size the total size of the progress bar in characters
+     * @param filledChar the character used to represent a filled unit of the bar
+     * @param unfilledChar the character used to represent an unfilled unit of the bar
+     * @param showPercentageFlag if true the percentage will be included after the progress bar
+     */
     public ProgressBar(int size, char filledChar, char unfilledChar, boolean showPercentageFlag) {
-        if (size > 100)
+        if (size > 100) {
             throw new IllegalArgumentException("size cannot exceed 100");
+        }
 
         this.size = size;
-        this.unitVal = 100.0 / size;
         this.filledChar = filledChar;
         this.unfilledChar = unfilledChar;
         this.showPercentageFlag = showPercentageFlag;
     }
 
+    /**
+     * Shows an ASCII representation of the progress bar with the given percentage.
+     *
+     * @param percent the percent filled
+     * @return a string representing the progress bar
+     */
     public String show(double percent) {
+        if (percent > 100) {
+            throw new IllegalArgumentException("percent cannot exceed 100");
+        }
+
         StringBuilder bar = new StringBuilder();
-
-        for (double cnt = unitVal; cnt <= percent; cnt = cnt + unitVal) {
-            bar.append(filledChar);
-        }
-
-        while (bar.length() < size) {
-            bar.append(unfilledChar);
-        }
+        int filled = calculateFilledUnits(percent);
+        bar.append(String.valueOf(filledChar).repeat(filled));
+        bar.append(String.valueOf(unfilledChar).repeat(size - filled));
 
         if (showPercentageFlag) {
             bar.append(" ").append((int) percent).append("%");
@@ -69,11 +102,30 @@ public class ProgressBar {
         return bar.toString();
     }
 
+    /**
+     * Shows an ASCII representation of the progress bar based on the given values.
+     *
+     * @param progress the current progress to display
+     * @param complete the value representing a completed bar
+     * @return a string representing the progress bar
+     */
     public String show(double progress, double complete) {
-        if (progress > complete)
+        if (progress > complete) {
             throw new IllegalArgumentException("progress cannot exceed complete");
+        }
 
         double percent = (progress / complete) * 100;
         return this.show(percent);
+    }
+
+    /**
+     * Calculates the number of filled units for this bar based on the given percentage.
+     * This will round the number of filled units to the nearest integer.
+     *
+     * @param percent the completion percentage to be represented
+     * @return the number of units to fill for this bar, rounded to the nearest whole number
+     */
+    private int calculateFilledUnits(double percent) {
+        return (int) Math.rint(size * (percent / 100));
     }
 }
