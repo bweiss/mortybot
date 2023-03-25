9fcd31756286d33e22d99d8518ea1ce03610a470
Brian Weiss <brianmweiss@gmail.com>
2023-03-25 15:43:39 -0400

    Add NICK command

---
+++ src/main/java/net/hatemachine/mortybot/commands/NickCommand.java
@@ -0,0 +1,73 @@
+/*
+ * MortyBot - An IRC bot built on the PircBotX framework.
+ * Copyright © 2022 Brian Weiss (brian@hatemachine.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.hatemachine.mortybot.commands;
+
+import net.hatemachine.mortybot.BotCommand;
+import net.hatemachine.mortybot.Command;
+import net.hatemachine.mortybot.MortyBot;
+import net.hatemachine.mortybot.exception.CommandException;
+import net.hatemachine.mortybot.listeners.CommandListener;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import java.util.List;
+
+/**
+ * Implements the NICK command, allowing users to change the bot's nickname.
+ */
+@BotCommand(name = "NICK", restricted = true, help = {
+        "Attempts to change the bot's nickname",
+        "Usage: NICK <newnick>"
+})
+public class NickCommand implements Command {
+
+    private final GenericMessageEvent event;
+    private final CommandListener.CommandSource source;
+    private final List<String> args;
+
+    public NickCommand(GenericMessageEvent event, CommandListener.CommandSource source, List<String> args) {
+        this.event = event;
+        this.source = source;
+        this.args = args;
+    }
+
+    @Override
+    public void execute() {
+        if (args.isEmpty()) {
+            throw new CommandException(CommandException.Reason.INVALID_ARGS, "Not enough arguments");
+        }
+
+        String newNick = args.get(0);
+        MortyBot bot = event.getBot();
+        bot.sendIRC().changeNick(newNick);
+    }
+
+    @Override
+    public GenericMessageEvent getEvent() {
+        return event;
+    }
+
+    @Override
+    public CommandListener.CommandSource getSource() {
+        return source;
+    }
+
+    @Override
+    public List<String> getArgs() {
+        return args;
+    }
+}
