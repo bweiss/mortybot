f2e95dabd288d179458ac32fbefbabaabbad18b8
Brian Weiss <brianmweiss@gmail.com>
2023-07-26 22:14:23 -0400

    Package reorg: move imdb into services

--- src/main/java/net/hatemachine/mortybot/services/imdb/IMDBHelper.java
+++ src/main/java/net/hatemachine/mortybot/services/imdb/IMDBHelper.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package net.hatemachine.mortybot.imdb;
+package net.hatemachine.mortybot.services.imdb;
 
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
--- src/main/java/net/hatemachine/mortybot/services/imdb/Person.java
+++ src/main/java/net/hatemachine/mortybot/services/imdb/Person.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package net.hatemachine.mortybot.imdb;
+package net.hatemachine.mortybot.services.imdb;
 
 import java.util.Objects;
 
